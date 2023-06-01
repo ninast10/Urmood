449bc290ec7a6a303d9363f234d1bc05553128dc
LaninaAstrid <134910002+LaninaAstrid@users.noreply.github.com>
2023-06-01 13:28:38 +0700

    Update app.js
    remove database connection due to undetected issue during deployment

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const connection = require('./database.js');
+//const connection = require('./database.js');
 //const mysql = require('mysql');
-const db = require('./connection');
+//const db = require('./connection');
 //const response =require('./response');
 
 app.use(bodyParser.json());
@@ -21,87 +21,143 @@ app.get('/', (req, res) => {
     res.send( "connect");
 });
 
+// const pool = mysql.createConnection({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+// });
 
-//GET all tips ramdomly
-app.get('/tips/:alltips', (req, res) => {
-    const sql = "SELECT * FROM tipstips order by rand()";
-    db.query(sql, [req.params.tipstips], (error, result) => {
-       if (result == 0) {
-        res.send({status: 'not found'});
-       } else{
-        res.send(result);
+//GET all tips
+app.get('/:tips', (req, res) => {
+     res.send({
+        data: {
+            tips: 'Open, accepting, and loving toward yourself and what you are going through, consider a simple exercise such as wallk, try something new, accept and validate your emotions, and getting enough sleep'
+        }
+     });
+});
+
+//GET tips by Id
+app.get('/tips/1', (req, res) => {
+    res.send({
+       data: {
+           tips: 'Recognize and appreciate your goals achievement'
        }
     });
 });
 
-//GET the tips by Id
-app.get('/tips/:Id', (req, res) => {
-    const sql = "SELECT * FROM tipstips Where Id = ?";
-    db.query(sql, [req.params.Id], (error, result) => {
-       if (result == 0) {
-        res.send({status: 'not found'});
-       } else{
-        res.send(result);
+app.get('/tips/2', (req, res) => {
+    res.send({
+       data: {
+           tips: 'Do something that is pleasurable or meaningful'
        }
     });
 });
 
-//GET all solutions
-app.get('/:solution', (req, res) => {
-    const sql = "SELECT * FROM solution";
-    db.query(sql, [req.params.solution], (error, result) => {
-       if (result == 0) {
-        res.send({status: 'not found'});
-       } else{
-        res.send(result);
+app.get('/tips/3', (req, res) => {
+    res.send({
+       data: {
+           tips: 'Listen to your favorite music'
        }
     });
 });
 
-//GET all the solutions by Id
-app.get('/solution/:Id', (req, res) => {
-    const sql = "SELECT * FROM solution Where Id = ?";
-    db.query(sql, [req.params.Id], (error, result) => {
-       if (result == 0) {
-        res.send({status: 'not found'});
-       } else{
-        res.send(result);
+app.get('/tips/4', (req, res) => {
+    res.send({
+       data: {
+           tips: 'Spending time in nature'
        }
     });
 });
 
-//GET the article link by Id
-app.get('/solution/article/:Id', (req, res) => {
-    const sql = "SELECT article FROM solution Where Id = ?";
-    db.query(sql, [req.params.Id], (error, result) => {
-       if (result == 0) {
-        res.send({status: 'not found'});
-       } else{
-        res.send(result);
+app.get('/tips/5', (req, res) => {
+    res.send({
+       data: {
+           tips: 'Writing or journaling about what you are experiencing'
        }
     });
 });
+
+//GET aerticle by Id
+app.get('/article/1', (req, res) => {
+    res.send({
+        data: {
+            article: 'https://scholarworks.calstate.edu/downloads/5712mc40c'
+        }
+     });
+});
+
+app.get('/article/2', (req, res) => {
+    res.send({
+        data: {
+            article: 'https://pdfs.semanticscholar.org/1e57/098b398a70ea7f965af43e02e9e78fa4c1b0.pdf'
+        }
+     });
+});
+
+app.get('/article/3', (req, res) => {
+    res.send({
+        data: {
+            article: 'https://onlinelibrary.wiley.com/doi/10.1002/mhw.31471'
+        }
+     });
+});
+
+app.get('/article/4', (req, res) => {
+    res.send({
+        data: {
+            article: 'https://academic.oup.com/pcm/article/3/3/161/5861360'
+        }
+     });
+});
+
+app.get('/article/5', (req, res) => {
+    res.send({
+        data: {
+            article: 'https://www.jahonline.org/action/showPdf?pii=S1054-139X%2818%2930479-8'
+        }
+     });
+});
+
+
 
 //GET the consultation contact by Id
-app.get('/solution/contact/:Id', (req, res) => {
-    const sql = "SELECT contact FROM solution Where Id = ?";
-    db.query(sql, [req.params.Id], (error, result) => {
-       if (result == 0) {
-        res.send({status: 'not found'});
-       } else{
-        res.send(result);
-       }
-    });
+app.get('/contact/1', (req, res) => {
+    res.send({
+        data: {
+            contact: 'https://cmlabs.co/id-id/promo/free-consultation-benefit'
+        }
+     });
 });
 
+app.get('/contact/2', (req, res) => {
+    res.send({
+        data: {
+            contact: 'http://www.brainfit.co.id/free-consultation-2/'
+        }
+     });
+});
 
-// app.post('/login', (req, res) => {
-//     console.log({ reqfrmOut: req.body})
-//     res.send('login berhasil')
-// })
+app.get('/contact/3', (req, res) => {
+    res.send({
+        data: {
+            contact: 'https://hopeforhealingfoundation.org'
+        }
+     });
+});
 
-// app.put('/username', (req, res) => {
-//     console.log({updateData: req.body})
-//     res.send('update berhasil')
-// })
+app.get('/contact/4', (req, res) => {
+    res.send({
+        data: {
+            contact: 'https://www.aeccglobal.co.id/id/contact-us/'
+        }
+     });
+});
 
+app.get('/contact/5', (req, res) => {
+    res.send({
+        data: {
+            contact: 'https://www.ibunda.id/'
+        }
+     });
+});
