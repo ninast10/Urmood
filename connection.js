e789c2da94a9a12056f00ba341e6c5b5f3a52c15
Lanina Astrid Chrysant Vrij <125802085+ninast10@users.noreply.github.com>
2023-11-05 16:04:18 +0700

    connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
-module.exports = connection;
+db.connect((err) => {
+  if (err) {
+    console.error('connection failed: ', err);
+    return;
+  }
+  console.log('Connect to MySQL');
+});
+
+module.exports = db;
